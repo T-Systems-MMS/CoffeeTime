365ee6109b9c3569875dfdb43096990fbb24c814
Thomas Kalmar <th.kalmar@gmail.com>
2018-10-09 12:12:25 +0200

    Fix tests

--- frontend/src/app/data.service.spec.ts
+++ frontend/src/app/data.service.spec.ts
@@ -1,9 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DataService } from './data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('DataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule
+    ],
+    providers: [
+      DataService
+    ]
+  }));
 
   it('should be created', () => {
     const service: DataService = TestBed.get(DataService);
