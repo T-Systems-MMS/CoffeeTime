886310d9585a1cf80b3d6f934127287eb1637700
thomasKalmar <th.kalmar@gmail.com>
2018-10-09 15:43:59 +0200

    Feature/#11 heroku support (#13)
    * Add initial package.json
    
    * Prepare external Port definition

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  Logger.log(`Listening on port ${port}`);
+  await app.listen(port);
 }
 bootstrap();
