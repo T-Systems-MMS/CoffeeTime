0c938766e7c613dad5f7750661b9f42c927d2d57
Thomas Kalmar <th.kalmar@gmail.com>
2018-10-10 11:27:14 +0200

    Add all api methods as stub

--- backend/src/api.controller.ts
+++ backend/src/api.controller.ts
@@ -1,13 +1,41 @@
-import { Get, Controller,  Headers, Logger } from '@nestjs/common';
+import { Get, Controller,  Headers, Logger, Put, Param, HttpException, Body, Post, Delete } from '@nestjs/common';
 import { RoomService } from './room.service';
 import { Room } from './domain/room';
+import { Push } from './domain/push';
+import { PushSubscription } from './domain/pushsubscription';
+import { PushUnSubscription } from './domain/pushunsubscription';
 
 @Controller()
 export class ApiController {
-  constructor(private readonly appService: RoomService) {}
+  private static AUTH_HEADER_NAME = 'PUSH_SUBSCRIPTION_AUTH'.toLowerCase();
+  constructor(private readonly roomService: RoomService) {}
 
   @Get('/api/rooms')
-  rooms(@Headers() headers): Array<Room> {
-    return this.appService.rooms();
+  rooms(@Headers(ApiController.AUTH_HEADER_NAME) subscriptionAuth): Array<Room> {
+    return this.roomService.rooms();
+  }
+
+  @Put('/api/room/:room_id/push')
+  addPush(@Param('room_id') id, @Headers(ApiController.AUTH_HEADER_NAME) subscriptionAuth, @Body() push: Push ): void {
+    if (!subscriptionAuth){
+      throw new HttpException('Missing Auth Header', 400);
+    }
+    Logger.log(`Got Room id: ${id}`);
+    Logger.log(`Body: ${push}`);
+  }
+
+  @Get('/api/room/:room_id')
+  getRoomInfo(@Param('room_id') roomId){
+    return this.roomService.room(roomId);
+  }
+
+  @Post('/api/push')
+  createPush(@Body() pushSubscription: PushSubscription){
+    Logger.log(`Body: ${pushSubscription}`);
+  }
+
+  @Delete('/api/push')
+  deletePush(@Body() pushUnSubscription: PushUnSubscription){
+    Logger.log(`Body: ${pushUnSubscription}`);
   }
 }
--- backend/src/room.service.ts
+++ backend/src/room.service.ts
@@ -6,4 +6,12 @@ export class RoomService {
   rooms(): Array<Room> {
     return [new Room('123'), new Room('456')];
   }
+  room(roomId: string){
+    return {
+      name: 'Raum der Stille',
+      history: [{ timestamp: new Date().getTime() - 40000, occupancy: 0.475 }],
+      averageWaitingTime: 124,
+      averageOccupancy: 0.475,
+     };
+  }
 }
