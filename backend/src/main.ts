99db4a7f7be6d20ea05a552df0351bc6fbb416f9
thomasKalmar <th.kalmar@gmail.com>
2018-10-12 14:54:19 +0200

    Bugfix/#36 redirect auf root (#38)
    * Query AWS be
    
    - use request to query the REST be
    - transform the data
    - call storage service
    
    * Redirect 404, Close #36
    
    * Cleanup and Fixes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { join } from 'path';
+import { NotFoundExceptionFilter } from 'notfound-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,10 +10,12 @@ async function bootstrap() {
    join(__dirname, '..', '..', 'frontend', 'dist', 'frontend'),
    {
       prefix: '/',
+      fallthrough: true,
    },
   );
   const port = process.env.PORT || 3000;
   Logger.log(`Listening on port ${port}`);
+  app.useGlobalFilters(new NotFoundExceptionFilter());
   await app.listen(port);
 }
 bootstrap();
