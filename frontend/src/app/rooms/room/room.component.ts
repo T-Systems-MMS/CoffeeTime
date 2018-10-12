392113cd3ead5ac681a6199dd3eab350232d1b48
malaupa <Stefan.Schubert@t-systems.com>
2018-10-12 08:10:49 +0200

    rebase and fixes

--- frontend/src/app/rooms/room/room.component.ts
+++ frontend/src/app/rooms/room/room.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, Input, Inject } from '@angular/core';
 import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
-import { SwPush } from '@angular/service-worker';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
-import { viewParentEl } from '@angular/core/src/view/util';
-import { PushService } from '../push.service';
 import { RoomService } from '../room.service';
 import { Router } from '@angular/router';
 import { Interpolation, FixedScaleAxis } from 'chartist';
@@ -62,36 +59,36 @@ export class RoomComponent implements OnInit {
 
     @Input() room;
 
-  constructor(
-    private service: RoomService,
-    private router: Router,
-    @Inject(LOCAL_STORAGE) private storage: StorageService
-  ) { }
+    constructor(
+        private service: RoomService,
+        private router: Router,
+        @Inject(LOCAL_STORAGE) private storage: StorageService
+    ) { }
 
-    ngOnInit() {
+    ngOnInit() { }
 
-  public toogleFavorite() {
-    this.room.favorite = !this.room.favorite;
+    public toogleFavorite() {
+        this.room.favorite = !this.room.favorite;
 
-    // get favorites from storage and remove current room-id if present
-    const favorites = (this.storage.get(STORGE_KEY) || []).filter(id => id !== this.room.id);
+        // get favorites from storage and remove current room-id if present
+        const favorites = (this.storage.get(STORGE_KEY) || []).filter(id => id !== this.room.id);
 
-    if (this.room.favorite) {
-      favorites.push(this.room.id);
+        if (this.room.favorite) {
+            favorites.push(this.room.id);
+        }
+
+        this.storage.set(STORGE_KEY, favorites);
     }
 
-    this.storage.set(STORGE_KEY, favorites);
-  }
+    public toogleIfFreePush(event: MatSlideToggleChange) {
+        this.service.togglePush(this.room, 'ifFree', event.checked);
+    }
 
-  public toogleIfFreePush(event: MatSlideToggleChange) {
-    this.service.togglePush(this.room, 'ifFree', event.checked);
-  }
+    public toogleRecommendationsPush(event: MatSlideToggleChange) {
+        this.service.togglePush(this.room, 'recommendations', event.checked);
+    }
 
-  public toogleRecommendationsPush(event: MatSlideToggleChange) {
-    this.service.togglePush(this.room, 'recommendations', event.checked);
-  }
-
-  public gotoRoomDetails() {
-    this.router.navigateByUrl(`/room/${this.room.id}`);
-  }
+    public gotoRoomDetails() {
+        this.router.navigateByUrl(`/room/${this.room.id}`);
+    }
 }
