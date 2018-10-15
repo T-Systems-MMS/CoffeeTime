1433959f8443d233b3e45b5645c97107f182af22
malaupa <Stefan.Schubert@t-systems.com>
2018-10-15 12:57:10 +0200

    fix transition; introduce new frame component; fix chart type attribute

--- frontend/src/app/animations.ts
+++ frontend/src/app/animations.ts
@@ -7,7 +7,31 @@ import {
 // Routable animations
 export const slideInAnimation =
     trigger('routeAnimation', [
-        transition('rooms <=> room', [
+        transition('rooms => room', [
+            style({ position: 'relative' }),
+            query(':enter, :leave', [
+                style({
+                    position: 'absolute',
+                    top: 0,
+                    right: 0,
+                    width: '100%'
+                })
+            ]),
+            query(':enter', [
+                style({ right: '-100%' })
+            ]),
+            query(':leave', animateChild()),
+            group([
+                query(':leave', [
+                    animate('300ms ease-out', style({ right: '100%' }))
+                ]),
+                query(':enter', [
+                    animate('300ms ease-out', style({ right: '0%' }))
+                ])
+            ]),
+            query(':enter', animateChild()),
+        ]),
+        transition('room => rooms', [
             style({ position: 'relative' }),
             query(':enter, :leave', [
                 style({
