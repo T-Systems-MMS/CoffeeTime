7cc68574dae3ab4d2793d071ca316d5192666ecc
malaupa <Stefan.Schubert@t-systems.com>
2018-10-15 20:20:49 +0200

    add compression and ssl redirect; activate frontend prodbuild

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,19 +3,23 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { join } from 'path';
 import { NotFoundExceptionFilter } from 'notfound-exception.filter';
+import * as compression from 'compression';
+import * as sslRedirect from 'heroku-ssl-redirect';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useStaticAssets(
-   join(__dirname, '..', '..', 'frontend', 'dist', 'frontend'),
-   {
-      prefix: '/',
-      fallthrough: true,
-   },
-  );
-  const port = process.env.PORT || 3000;
-  Logger.log(`Listening on port ${port}`);
-  app.useGlobalFilters(new NotFoundExceptionFilter());
-  await app.listen(port);
+    const app = await NestFactory.create(AppModule);
+    app.use(sslRedirect());
+    app.use(compression());
+    app.useStaticAssets(
+        join(__dirname, '..', '..', 'frontend', 'dist', 'frontend'),
+        {
+            prefix: '/',
+            fallthrough: true,
+        },
+    );
+    const port = process.env.PORT || 3000;
+    Logger.log(`Listening on port ${port}`);
+    app.useGlobalFilters(new NotFoundExceptionFilter());
+    await app.listen(port);
 }
 bootstrap();
