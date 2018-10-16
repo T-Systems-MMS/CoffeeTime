4c531443950bc3e536a1626c4d07e2da6b561d09
Stefan Schubert <22217677+malaupa@users.noreply.github.com>
2018-10-16 10:42:36 +0200

    attach chart; add timer for polling (#55)
    * attach chart; add timer for polling
    
    * disable test

--- frontend/src/app/rooms/room-detail/room-detail.component.ts
+++ frontend/src/app/rooms/room-detail/room-detail.component.ts
@@ -5,77 +5,43 @@ import { Interpolation, FixedScaleAxis } from 'chartist';
 import { thresholdPlugin } from '../../../components/threshold.plugin';
 import { Room } from '../room';
 import { Subscription } from 'rxjs';
+import { RoomListComponent } from '../room-list/room-list.component';
+import { map } from 'rxjs/operators';
+import { formatDate } from '@angular/common';
 
 @Component({
-  selector: 'app-room-detail',
-  templateUrl: './room-detail.component.html',
-  styleUrls: ['./room-detail.component.scss']
+    selector: 'app-room-detail',
+    templateUrl: './room-detail.component.html',
+    styleUrls: ['./room-detail.component.scss']
 })
 export class RoomDetailComponent implements OnInit, OnDestroy {
-  room: Room;
-  private _subscription: Subscription;
+    room: Room;
+    private _subscription: Subscription;
 
-    data = {
-        series: [{
-            name: 'filling',
-            data: [
-                { x: new Date('2017-12-27T12:30:00'), y: 20 },
-                { x: new Date('2017-12-27T12:31:00'), y: 95 },
-                { x: new Date('2017-12-27T12:32:00'), y: 80 },
-                { x: new Date('2017-12-27T12:33:00'), y: 30 },
-                { x: new Date('2017-12-27T12:34:00'), y: 10 },
-                { x: new Date('2017-12-27T12:41:00'), y: 30 },
-                { x: new Date('2017-12-27T12:43:00'), y: 30 },
-                { x: new Date('2017-12-27T12:43:00'), y: 20 },
-                { x: new Date('2017-12-27T12:54:00'), y: 20 },
-            ]
-        }]
-    };
-    options = {
-        height: 100,
-        fullWidth: true,
-        axisX: {
-            type: FixedScaleAxis,
-            divisor: 3,
-            labelInterpolationFnc: (value: number) => new Date(value).getHours() + ':' + new Date(value).getMinutes()
-        },
-        axisY: {
-            type: FixedScaleAxis,
-            ticks: [0, 50, 100],
-            low: 0,
-            high: 100,
-            showLabel: false,
-            offset: 0
-        },
-        lineSmooth: Interpolation.step(),
-        showPoint: false,
-        showArea: true,
-        plugins: [
-            thresholdPlugin({ thresholds: [15, 60] }),
-        ]
-    };
-    responsiveOptions = [];
+    options = RoomListComponent.getChartOptions();
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private service: RoomService
-  ) {
-    this.room = null;
-  }
-
-  ngOnInit(): void {
-    const id: string = this.route.snapshot.paramMap.get('id');
-    this._subscription = this.service.getRoom(id).subscribe(r => this.room = r);
-   }
-
-  ngOnDestroy(): void {
-    if (this._subscription) {
-      this._subscription.unsubscribe();
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private service: RoomService
+    ) {
+        this.room = null;
     }
-  }
 
-  gotoRooms(): void {
-    this.router.navigate(['/rooms']);
-  }
+    ngOnInit(): void {
+        const id: string = this.route.snapshot.paramMap.get('id');
+        this._subscription = this.service.getRoom(id)
+            .pipe(map(room => RoomListComponent.mapForecastOrHistory(room)))
+            .subscribe(room => this.room = room);
+    }
+
+    ngOnDestroy(): void {
+        if (this._subscription) {
+            this._subscription.unsubscribe();
+        }
+    }
+
+    gotoRooms(): void {
+        this.router.navigate(['/rooms']);
+    }
 }
--- frontend/src/app/rooms/room/room.component.ts
+++ frontend/src/app/rooms/room/room.component.ts
@@ -3,10 +3,9 @@ import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { RoomService } from '../room.service';
 import { Router } from '@angular/router';
-import { Interpolation, FixedScaleAxis } from 'chartist';
-import { verticalLinePlugin } from '../../../components/verticalline.plugin';
-import { thresholdPlugin } from '../../../components/threshold.plugin';
+
 import { Room } from '../room';
+import { RoomListComponent } from '../room-list/room-list.component';
 
 const STORGE_KEY = 'ct_favorites';
 @Component({
@@ -16,49 +15,10 @@ const STORGE_KEY = 'ct_favorites';
 })
 export class RoomComponent implements OnInit {
 
-    data = {
-        series: [{
-            name: 'filling',
-            data: [
-                { x: new Date('2017-12-27T12:30:00'), y: 20 },
-                { x: new Date('2017-12-27T12:31:00'), y: 95 },
-                { x: new Date('2017-12-27T12:32:00'), y: 80 },
-                { x: new Date('2017-12-27T12:33:00'), y: 30 },
-                { x: new Date('2017-12-27T12:34:00'), y: 10 },
-                { x: new Date('2017-12-27T12:41:00'), y: 30 },
-                { x: new Date('2017-12-27T12:43:00'), y: 30 },
-                { x: new Date('2017-12-27T12:43:00'), y: 20 },
-                { x: new Date('2017-12-27T12:54:00'), y: 20 },
-            ]
-        }]
-    };
-    options = {
-        height: 100,
-        fullWidth: true,
-        axisX: {
-            type: FixedScaleAxis,
-            divisor: 3,
-            labelInterpolationFnc: (value: number) => new Date(value).getHours() + ':' + new Date(value).getMinutes()
-        },
-        axisY: {
-            type: FixedScaleAxis,
-            ticks: [0, 50, 100],
-            low: 0,
-            high: 100,
-            showLabel: false,
-            offset: 0
-        },
-        lineSmooth: Interpolation.step(),
-        showPoint: false,
-        showArea: true,
-        plugins: [
-            thresholdPlugin({ thresholds: [15, 60] }),
-            verticalLinePlugin({ label: 'jetzt', position: new Date('2017-12-27T12:37:00').getTime(), className: 'ct-now' })
-        ]
-    };
-    responsiveOptions = [];
+    options = RoomListComponent.getChartOptions(true);
 
-    @Input() room: Room;
+    @Input()
+    room: Room;
 
     constructor(
         private service: RoomService,
