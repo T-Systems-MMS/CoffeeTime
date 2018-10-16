e3622dbee4ced109b40a0920c7382f8439ffbd30
malaupa <Stefan.Schubert@t-systems.com>
2018-10-16 11:18:08 +0200

    remove uneeded dependency; apply own middleware

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,20 @@ import { Logger } from '@nestjs/common';
 import { join } from 'path';
 import { NotFoundExceptionFilter } from 'notfound-exception.filter';
 import * as compression from 'compression';
-import * as sslRedirect from 'heroku-ssl-redirect';
+
+function sslRedirect(environments = ['production'], status = 302) {
+    return function (req, res, next) {
+        // on heroku NODE_ENV defaults to 'production'
+        if (environments.indexOf(process.env.NODE_ENV) >= 0) {
+            // look at headers to identify proto (https://devcenter.heroku.com/articles/http-routing#heroku-headers)
+            if (req.headers['x-forwarded-proto'] != 'https') {
+                res.redirect(status, 'https://' + req.hostname + req.originalUrl);
+                return;
+            }
+        }
+        next();
+    };
+};
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
