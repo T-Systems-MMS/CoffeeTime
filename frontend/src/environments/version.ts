38cd514d9d339e66187817eb5528ac701442b2b9
malaupa <Stefan.Schubert@t-systems.com>
2018-10-22 14:25:33 +0200

    fix version determination

--- frontend/src/environments/version.ts
+++ frontend/src/environments/version.ts
@@ -1,14 +1,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "6c53977-dirty",
-    "hash": "6c53977",
-    "distance": null,
-    "tag": null,
-    "semver": null,
-    "suffix": "6c53977-dirty",
-    "semverString": null,
+    "hash": "78ed45f",
     "version": "1.0.0"
 };
 /* tslint:enable */
--- frontend/version.js
+++ frontend/version.js
@@ -3,19 +3,26 @@ const { version } = require('./package.json');
 const { resolve, relative } = require('path');
 const { writeFileSync } = require('fs-extra');
 
-const gitInfo = gitDescribeSync({
-    dirtyMark: false,
-    dirtySemver: false
-});
+const versionInfo = {};
+try {
+    versionInfo.hash = gitDescribeSync({
+        dirtyMark: false,
+        dirtySemver: false
+    }).hash;
+} catch(e) {
+    console.log(e);
+    // we are on heroku...
+    versionInfo.hash = process.env['COMMIT_HASH'];
+}
 
-gitInfo.version = version;
+versionInfo.version = version;
 
 const file = resolve(__dirname, 'src', 'environments', 'version.ts');
 writeFileSync(file,
 `// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
-export const VERSION = ${JSON.stringify(gitInfo, null, 4)};
+export const VERSION = ${JSON.stringify(versionInfo, null, 4)};
 /* tslint:enable */
 `, { encoding: 'utf-8' });
 
-console.log(`Wrote version info ${gitInfo.hash} to ${relative(__dirname, file)}`);
+console.log(`Wrote version info ${versionInfo.hash} to ${relative(__dirname, file)}`);
