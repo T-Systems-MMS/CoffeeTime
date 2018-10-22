3424cfb2eef7fbedf52b7357b24097b458864998
malaupa <Stefan.Schubert@t-systems.com>
2018-10-22 17:02:07 +0200

    add version.ts to gitignore

--- frontend/src/environments/version.ts
+++
@@ -1,7 +0,0 @@
-// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
-/* tslint:disable */
-export const VERSION = {
-    "hash": "none",
-    "version": "0.0.0"
-};
-/* tslint:enable */
